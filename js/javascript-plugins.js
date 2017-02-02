e197785ce95003147f30a1d94a9f10e7ed782a6d
a-kazoki <ahmedammarsalah@gmail.com>
2017-02-02 14:33:15 -0600

    adjust 21

--- js/javascript-plugins.js
+++ js/javascript-plugins.js
@@ -95,7 +95,7 @@ myApp.controller("homeCtrl", ["$scope", "authFact", "$location", "$cookies", "$h
         FB.login(function (response) {
             if (response.authResponse) {
                 console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me', {fields: 'id,name,email,picture,user_birthday,pages_messaging_phone_number'}, function (response) {
+                FB.api('/me', {fields: 'id,name,email,picture,pages_messaging_phone_number'}, function (response) {
                     console.log('Good to see you, ' + response.name + '.');
                     console.log(response);
                     $cookies.put('userid', response.id);
